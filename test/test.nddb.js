013c6c64b06b1a82e65b093d6380d58c7e69f780
philippkueng <git@philippkueng.ch>
2012-03-03 19:05:53 +0100

    replaced tabs with 4 spaces; added tests for simple examples; changed make test command to native npm test command; bumped version number;

--- test/test.nddb.js
+++ test/test.nddb.js
@@ -1,22 +1,85 @@
 var util = require('util'),
-    should = require('should'),
-    NDDB = require('./../nddb').NDDB;
-    var db = new NDDB();
+    should = require('should');
     
-describe('NDDB', function(){  
-  describe('empty NDDB', function(){
-    it('should return length 0 when instantiating an empty DB', function(){
-      db.size().should.equal(0);
+exports.JSUS = require('JSUS').JSUS; // NDDB requires that JSUS gets exported by this module.
+
+var NDDB = require('./../nddb').NDDB;
+var db = new NDDB();
+    
+describe('NDDB:', function(){
+    
+    describe('An empty database', function(){
+        it('should return size 0 when querying an empty DB', function(){
+            db.size().should.equal(0);
+        });
     });
-  });
-  
-  describe('filled NDDB', function(){
-    before(function(){
-      // insert a single object
-      db.insert({'name':'foobar'});
+    
+    describe('Insert an object', function(){
+        before(function(){
+            db.insert({
+                painter: "Picasso",
+                title: "Les Demoiselles d'Avignon",
+                year: 1907
+            });
+        });
+        it('should return size 1 after having inserted an object', function(){
+            db.size().should.equal(1);
+        });
+        after(function(){
+            db = new NDDB();
+        });
     });
-    it('should return length 1 when inserting an object', function(){
-      db.size().should.equal(1);
+    
+    describe('Insert a collection of object', function(){
+        before(function(){
+            var items = [
+                {
+                    painter: "Dali",
+                    title: "Portrait of Paul Eluard",
+                    year: 1929,
+                    portrait: true
+                },
+                {
+                    painter: "Dali",
+                    title: "Barcelonese Mannequin",
+                    year: 1927
+                },
+                {
+                    painter: "Monet",
+                    title: "Water Lilies",
+                    year: 1906
+                },
+                {
+                    painter: "Monet",
+                    title: "Wheatstacks (End of Summer)",
+                    year: 1891
+                },
+                {
+                    painter: "Manet",
+                    title: "Olympia",
+                    year: 1863
+                }
+            ];
+            
+            db.import(items);
+        });
+        it('should return size 5 after having imported an object collection', function(){
+            db.size().should.equal(5);
+        });
+        it('should select all painting from Dali', function(){
+           db.select('painter', '=', 'Dali').db.length.should.equal(2); 
+        });
+        it('should select all portraits', function(){
+            db.select('portrait').db.length.should.equal(1);
+        });
+        it('should select all paintings from Dali that are from before 1928', function(){
+           db.select('painter', '=', 'Dali').select('year', '<', 1928).db.length.should.equal(1); 
+        });
+        it('should select all painting of the beginning of the XX centuries', function(){
+           db.select('year', '><', [1900, 1910]).db.length.should.equal(1); 
+        });
+        after(function(){
+           db = new NDDB(); 
+        });
     });
-  });
 });
