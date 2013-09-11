448e645e6d2e5c2ca572493b19ba3524b29e6274
Stefano Balietti <futur.dorko@gmail.com>
2013-09-11 13:56:52 +0200

    added test for inheritedclass

--- test/test.inheritedclass.js
+++ test/test.inheritedclass.js
@@ -1,7 +1,7 @@
 var util = require('util'),
 should = require('should'),
 NDDB = require('./../nddb').NDDB,
-JSUS = require('JSUS').JSUS;
+J = require('JSUS').JSUS;
 
 var db = new NDDB();
 
@@ -34,6 +34,27 @@ function ADB() {
     }); 
 }
 
+function ADB_confObj() {
+    var options = {};
+    options.update = {};
+    options.update.indexes = true;      
+    options.I = {
+        id: function(i) {
+            return i.id;
+        }
+    };
+    options.V = {
+        vi: function(i) {
+            return i.id;
+        }
+    };
+    options.H = {
+        ha: function(i) {
+            return i.id;
+        }
+    };
+    NDDB.call(this, options); 
+}
 
 var adb;
 
@@ -41,13 +62,72 @@ describe('ADB inherited class with view, index, and hash', function() {
     before(function() {
         adb = new ADB();
     });
+    describe("#constructor()",function() {
+    	
+        it("created obj should have empty index", function() {
+            ('undefined' !== typeof adb.id).should.be.true;
+        });
+        it("created obj should have empty view", function() {
+            ('undefined' !== typeof adb.vi).should.be.true;
+        });
+        it("created obj should have hash hash", function() {
+            ('undefined' !== typeof adb.ha).should.be.true;
+        });
+
+        it("created obj should have view with empty index", function() {
+            ('undefined' !== typeof adb.vi.id).should.be.true;
+        });
+        it("created obj should have view with empty hash", function() {
+            ('undefined' !== typeof adb.vi.ha).should.be.true;
+        });        
+        
+    });
 
     describe("#insert()",function() {
     	
         it("should return add 1 item to adb", function() {
             adb.insert({id: 1});
             adb.size().should.be.eql(1);
+        });    
+        it("hash should have non-empty index", function() {
+            ('undefined' !== typeof adb.ha[1].id).should.be.true;
+            adb.ha[1].id.size().should.be.eql(1);
+        });
+        it("hash should have non-empty view", function() {
+            ('undefined' !== typeof adb.ha[1].vi).should.be.true;
+            adb.ha[1].vi.size().should.be.eql(1);
+        });
 
+        it("view should have non-empty index", function() {
+            ('undefined' !== typeof adb.vi.id).should.be.true;
+            adb.vi.id.size().should.be.eql(1);
+        });
+        it("view should have non-empty hash", function() {
+            ('undefined' !== typeof adb.vi.ha[1]).should.be.true;
+            adb.vi.ha[1].size().should.be.eql(1);
+        });
+    });
+
+    describe("removing from index",function() {
+    	before(function() {
+            adb.id.pop(1);
+        });
+        it("should reduce db size to 0", function() {
+            adb.size().should.be.eql(0);
+        });
+        
+        it("should reduce view size to 0", function() {
+            adb.vi.size().should.be.eql(0);
+        });
+        it("should reduce index size in view to 0", function() {
+            adb.vi.id.size().should.be.eql(0);
+        });
+        it("should reduce hash size in view to 0", function() {
+            J.size(adb.vi.ha).should.be.eql(0);
+        });
+
+        it("should reduce hash size to 0", function() {
+            J.size(adb.ha).should.be.eql(0);
         });
     });
 });
