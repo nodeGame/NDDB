a91309f4936bca606dd927aef4cbb455bcd4db68
Stefano Balietti <futur.dorko@gmail.com>
2015-09-05 10:15:21 +0200

    fixing csv options

--- test/test.csv.js
+++ test/test.csv.js
@@ -44,23 +44,45 @@ describe('#load(".csv")', function(){
         });
     });
 
-//     it('should load a csv file with user-specified options', function(done) {
-//         db = new NDDB();
-//         options = {
-//             quote: '-'
-//         };
-//         db.load(filename, options, function() {
-//             db.size().should.eql(4);
-//             console.log(db.db);
-//             db.last().should.be.eql({
-//                 '"A"': '"10"',
-//                 '"B"': '"11"',
-//                 '"C"': '"12"',
-//                 '"D"': '"Z4"'
-//             });
-//             done();
-//         });
-//     });
+    it('should load a csv file with without quotes', function(done) {
+        db = new NDDB();
+        options = {
+            quote: '-'
+        };
+        db.load(filename, options, function() {
+            db.size().should.eql(4);
+            db.last().should.be.eql({
+                '"A"': '"10"',
+                '"B"': '"11"',
+                '"C"': '"12"',
+                '"D"': '"Z4"'
+            });
+            done();
+        });
+    });
+//    it('should load a csv file with pre-defined headers', function(done) {
+//        db = new NDDB();
+//        options = {
+//            headers: ['q', 'w', 'e', 'r']
+//        };
+//        db.load(filename, options, function() {
+//            console.log(db.db);
+//            db.size().should.eql(5);
+//            db.first().should.be.eql({
+//                q: 'A',
+//                w: 'B',
+//                e: 'C',
+//                r: 'D'
+//            });
+//            db.last().should.be.eql({
+//                A: "10",
+//                B: "11",
+//                C: "12",
+//                D: "Z4"
+//            });
+//            done();
+//        });
+//    });
 
 });
 
