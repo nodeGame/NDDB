e72d49414286e46699aea7444d92b47e98330784
Stefano Balietti <futur.dorko@gmail.com>
2015-09-07 10:50:01 +0200

    fix

--- test/test.csv.js
+++ test/test.csv.js
@@ -27,11 +27,41 @@ var lastItemUnescaped = {
     '"D"': '"Z4"'
 };
 
-describe('#load(".csv")', function(){
 
-    it('should load a csv file with default options', function(done) {
+var items = [
+    {
+        id: '123456',
+        time: '098765',
+        data: {
+            a: 1,
+            b: 2,
+            c: "she said: \"ah.\""
+        }
+    },
+    {
+        id: '123456',
+        time: '098765',
+        data: {
+            a: 1,
+            b: 2,
+            c: 'she said: "ah, not now."'
+        },
+    },
+];
+
+var ops = ['loadSync'];
+
+var i, len;
+i = -1, len = ops.length;
+
+
+function getTests(m, it) {
+
+    var db;
+
+    it('should ' + m + ' a csv file with default options', function(done) {
         db = new NDDB();
-        db.load(filename, function() {
+        db[m](filename, function() {
             db.size().should.eql(4);
             db.last().should.be.eql(lastItem);
             done();
@@ -39,32 +69,33 @@ describe('#load(".csv")', function(){
 
     });
 
-    it('should load a csv file with empty options', function(done) {
+    it('should ' + m + ' a csv file with empty options', function(done) {
         db = new NDDB();
-        db.load(filename, {}, function() {
+        db[m](filename, {}, function() {
             db.size().should.eql(4);
             db.last().should.be.eql(lastItem);
             done();
         });
     });
 
-    it('should load a csv file with without quotes', function(done) {
+    it('should ' + m + ' a csv file with without quotes', function(done) {
         db = new NDDB();
         options = {
             quote: '-'
         };
-        db.load(filename, options, function() {
+        db[m](filename, options, function() {
             db.size().should.eql(4);
             db.last().should.be.eql(lastItemUnescaped);
             done();
         });
     });
-    it('should load a csv file with pre-defined headers', function(done) {
+    it('should ' + m + ' a csv file with pre-defined headers', function(done) {
+
         db = new NDDB();
         options = {
             headers: ['q', 'w', 'e', 'r']
         };
-        db.load(filename, options, function() {
+        db[m](filename, options, function() {
             db.size().should.eql(5);
             db.first().should.be.eql({
                 q: 'A',
@@ -81,60 +112,13 @@ describe('#load(".csv")', function(){
             done();
         });
     });
+}
 
-    it('should load a csv file with pre-defined adapter', function(done) {
-        db = new NDDB();
-        options = {
-            headers: ['q', 'w', 'e', 'r']
-        };
-        db.load(filename, options, function() {
-            db.size().should.eql(5);
-            db.first().should.be.eql({
-                q: 'A',
-                w: 'B',
-                e: 'C',
-                r: 'D'
-            });
-            db.last().should.be.eql({
-                q: "10",
-                w: "11",
-                e: "12",
-                r: "Z4"
-            });
-            done();
-        });
-    });
-
+describe('#load(".csv")', function() {
+    getTests('load', it);
 });
 
-// describe('#loadSync(".csv")', function(){
-//
-//     it('should load sync. a csv file with default options', function(done) {
-//         db = new NDDB();
-//         db.loadSync(filename, function() {
-//             db.size().should.eql(4);
-//             db.last().should.be.eql({
-//                 A: "10",
-//                 B: "11",
-//                 C: "12",
-//                 D: "Z4"
-//             });
-//             done();
-//         });
-//
-//     });
-//
-//     it('should load sync. a csv file with empty options', function(done) {
-//         db = new NDDB();
-//         db.loadSync(filename, {}, function() {
-//             db.size().should.eql(4);
-//             db.last().should.be.eql({
-//                 A: "10",
-//                 B: "11",
-//                 C: "12",
-//                 D: "Z4"
-//             });
-//             done();
-//         });
-//     });
-// });
+
+describe('#loadSync(".csv")', function() {
+    getTests('loadSync', it);
+});
