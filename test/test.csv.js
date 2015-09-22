3a30bc6b673bb2ae7bd3d83e1d33e02cbfac2129
Stefano Balietti <futur.dorko@gmail.com>
2015-09-22 10:58:59 +0200

    csv test async/sync one function

--- test/test.csv.js
+++ test/test.csv.js
@@ -31,12 +31,35 @@ var lastItemUnescaped = {
 };
 
 
+var items = [
+    {
+        id: '123456',
+        time: '098765',
+        data: {
+            a: 1,
+            b: 2,
+            c: "she said: \"ah.\""
+        }
+    },
+    {
+        id: '123456',
+        time: '098765',
+        data: {
+            a: 1,
+            b: 2,
+            c: 'she said: "ah, not now."'
+        },
+    },
+];
 
-describe('#load(".csv")', function(){
 
-    it('should load a csv file with default options', function(done) {
+function getTests(m, it) {
+
+    var db;
+
+    it('should ' + m + ' a csv file with default options', function(done) {
         db = new NDDB();
-        db.load(filename.standard, function() {
+        db[m](filename.standard, function() {
             db.size().should.eql(4);
             db.last().should.be.eql(lastItem);
             done();
@@ -44,27 +67,51 @@ describe('#load(".csv")', function(){
 
     });
 
-    it('should load a csv file with empty options', function(done) {
+    it('should ' + m + ' a csv file with empty options', function(done) {
         db = new NDDB();
-        db.load(filename.standard, {}, function() {
+        db[m](filename.standard, {}, function() {
             db.size().should.eql(4);
             db.last().should.be.eql(lastItem);
             done();
         });
     });
 
-    it('should load a csv file with without quotes', function(done) {
+    it('should ' + m + ' a csv file with without quotes', function(done) {
         db = new NDDB();
         options = {
             quote: '-'
         };
-        db.load(filename.standard, options, function() {
+        db[m](filename.standard, options, function() {
             db.size().should.eql(4);
             db.last().should.be.eql(lastItemUnescaped);
             done();
         });
     });
-    it('should load a csv file with pre-defined headers', function(done) {
+    it('should ' + m + ' a csv file with pre-defined headers', function(done) {
+
+        db = new NDDB();
+        options = {
+            headers: ['q', 'w', 'e', 'r']
+        };
+        db[m](filename.standard, options, function() {
+            db.size().should.eql(5);
+            db.first().should.be.eql({
+                q: 'A',
+                w: 'B',
+                e: 'C',
+                r: 'D'
+            });
+            db.last().should.be.eql({
+                q: "10",
+                w: "11",
+                e: "12",
+                r: "Z4"
+            });
+            done();
+        });
+    });
+
+    it('should ' + m + ' a csv file with pre-defined adapter', function(done) {
         db = new NDDB();
         options = {
             headers: ['q', 'w', 'e', 'r']
@@ -87,31 +134,8 @@ describe('#load(".csv")', function(){
         });
     });
 
-    it('should load a csv file with pre-defined adapter', function(done) {
-        db = new NDDB();
-        options = {
-            headers: ['q', 'w', 'e', 'r']
-        };
-        db.load(filename.standard, options, function() {
-            db.size().should.eql(5);
-            db.first().should.be.eql({
-                q: 'A',
-                w: 'B',
-                e: 'C',
-                r: 'D'
-            });
-            db.last().should.be.eql({
-                q: "10",
-                w: "11",
-                e: "12",
-                r: "Z4"
-            });
-            done();
-        });
-    });
 
-
-    it('should load a csv file with default options and unescape seperators',
+    it('should ' + m + ' a csv file with def. options and unescape separators',
        function(done) {
            db = new NDDB();
            db.load(filename.escapeTesting, function() {
@@ -129,34 +153,54 @@ describe('#load(".csv")', function(){
 
        });
 
+   it('should ' + m + ' a csv file with pre-defined adapter', function(done) {
+        db = new NDDB();
+        options = {
+            headers: ['q', 'w', 'e', 'r']
+        };
+        db[m](filename.standard, options, function() {
+            db.size().should.eql(5);
+            db.first().should.be.eql({
+                q: 'A',
+                w: 'B',
+                e: 'C',
+                r: 'D'
+            });
+            db.last().should.be.eql({
+                q: "10",
+                w: "11",
+                e: "12",
+                r: "Z4"
+            });
+            done();
+        });
+    });
+
+
+    it('should ' + m + ' a csv file with def. options and unescape separators',
+       function(done) {
+           db = new NDDB();
+           db[m](filename.escapeTesting, function() {
+               db.size().should.eql(4);
+               db.first();
+               db.next();
+               db.next().should.be.eql({
+                   A: '7,5',
+                   B: '8',
+                   C: '9',
+                   D: 'Z3'
+               });
+               done();
+           });
+
+       });
+}
+
+describe('#load(".csv")', function() {
+    getTests('load', it);
 });
 
-describe('#loadSync(".csv")', function(){
 
-    it('should load sync a csv file with default options', function() {
-        db = new NDDB();
-        db.loadSync(filename.standard, function() {
-            db.size().should.eql(4);
-            db.last().should.be.eql({
-                A: "10",
-                B: "11",
-                C: "12",
-                D: "Z4"
-            });
-        });
-
-    });
-
-    it('should load sync a csv file with empty options', function() {
-        db = new NDDB();
-        db.loadSync(filename.standard, {}, function() {
-            db.size().should.eql(4);
-            db.last().should.be.eql({
-                A: "10",
-                B: "11",
-                C: "12",
-                D: "Z4"
-            });
-        });
-    });
+describe('#loadSync(".csv")', function() {
+    getTests('loadSync', it);
 });
